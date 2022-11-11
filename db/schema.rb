f61bd71a688091b27912ecf596056fdfbdcfe26b
Kdott360x <64315231+Kdott360x@users.noreply.github.com>
2022-11-11 17:35:40 -0500

    Sql Error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_03_195943) do
+ActiveRecord::Schema.define(version: 2022_11_11_204622) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_11_03_195943) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "providers"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
